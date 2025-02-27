80673aab3e7a8c4b0b54d2b481fd745caf1c6775
Oleksandr Kuzmenko <oleksandr.kuzmenko@corteva.com>
2025-02-27 12:33:29 +0200

    Fire letters from user tank

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import random
 from tkinter import *
 import random
 
+#root
 root = Tk()
 
 # Constants
@@ -9,22 +10,21 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 # Fullscreen
 screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight() - 50
+screen_height = root.winfo_screenheight() - 150
 root.geometry(f"{screen_width}x{screen_height}")
 
 #Canvas
 canvas = Canvas(root)
 canvas.config(width=screen_width, height=screen_height)
+canvas.grid(column=0, row=0)
 
 # Top line drawing
 canvas.create_line(0, 50, screen_width - 10, 50, width=1)
-canvas.grid(column=0, row=1)
 
 # Computer tank
 computer_tank = canvas.create_rectangle(0, 10, 100, 40, fill='gray')
-canvas.grid(column=0, row=0)
 
-
+# Computer tank movement
 def move_tank():
     new_x = random.randrange(0, screen_width - 100)
     current_x = int(canvas.coords(computer_tank)[0])
@@ -47,8 +47,9 @@ def move_tank():
 
     animate_tank_movement()
 
+# Computer tank letter funcs
 def shot_letter():
-    current_tank_position_x = canvas.coords(computer_tank)[0]
+    current_x = canvas.coords(computer_tank)[0]
     is_uppercase = random.choice([True, False])
     letter_symbol = random.choice(alphabet)
     letter_symbol_color = "#0000FF" # small letter color
@@ -58,8 +59,9 @@ def shot_letter():
         letter_symbol_color = "#FF0000" # big letter color
 
     # make letter coordinate dynamic, not hardcoded
-    letter = canvas.create_text(current_tank_position_x + 50, 60, text=letter_symbol, fill=letter_symbol_color)
+    letter = canvas.create_text(current_x + 50, 60, text=letter_symbol, fill=letter_symbol_color)
     move_letter(letter)
+
 def move_letter(letter):
     _, current_y = canvas.coords(letter)
 
@@ -69,5 +71,50 @@ def move_letter(letter):
         canvas.move(letter, 0, 10)
         canvas.after(100, move_letter, letter)
 
+# User tank
+user_tank = canvas.create_rectangle(screen_width / 2, screen_height - 130, screen_width / 2 + 100, screen_height - 85, fill='brown')
+
+def move_tank_left(event):
+    user_tank_x = canvas.coords(user_tank)[0]
+
+    if user_tank_x == 0:
+        return
+
+    canvas.move(user_tank, -10, 0)
+def move_tank_right(event):
+    user_tank_x = canvas.coords(user_tank)[0]
+
+    if user_tank_x == screen_width - 100:
+        return
+
+    canvas.move(user_tank, 10, 0)
+def fire_letter(event):
+    letter = event.char
+    letter_symbol_color = "#0000FF"  # small letter color
+
+    if letter.isupper():
+        letter_symbol_color = "#FF0000"  # big letter color
+
+    current_x = canvas.coords(user_tank)[0]
+    current_y= canvas.coords(user_tank)[1]
+
+    letter = canvas.create_text(current_x + 50, current_y - 10, text=letter, fill=letter_symbol_color)
+    move_fired_letter(letter)
+
+def move_fired_letter(letter):
+    _, current_y = canvas.coords(letter)
+
+    if current_y < 10:
+        canvas.delete(letter)
+    else:
+        canvas.move(letter, 0, -10)
+        canvas.after(100, move_fired_letter, letter)
+
+# User panel line
+canvas.create_line(0, screen_height - 80, screen_width - 10, screen_height - 80, width=1)
+
 root.after(1000, move_tank)
+root.bind("<Left>", move_tank_left)
+root.bind("<Right>", move_tank_right)
+root.bind("<Key>", fire_letter)
 root.mainloop()
