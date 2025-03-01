f6a2e590f11ff3f771f8f4ce6ecce201a0886ddc
Oleksandr Kuzmenko <oleksandr.kuzmenko@corteva.com>
2025-03-01 09:24:37 +0200

    Create an air defence

--- src/main.py
+++ src/main.py
@@ -28,6 +28,8 @@ computer_tank = canvas.create_rectangle(0, 10, 100, 40, fill='gray')
 falling_letters = []
 wall_cells = []
 radars = []
+small_rocket = ""
+big_rocket = ""
 
 # Computer tank movement
 def move_tank():
@@ -65,6 +67,11 @@ def shot_letter():
 
     # make letter coordinate dynamic, not hardcoded
     letter = canvas.create_text(current_x + 50, 60, text=letter_symbol, fill=letter_symbol_color)
+
+    # listeners for air defence
+    canvas.tag_bind(letter, "<Button-1>", lambda event: left_mouse_click(event, letter, is_uppercase))
+    canvas.tag_bind(letter, "<Button-3>", lambda event: right_mouse_click(event, letter, is_uppercase))
+
     falling_letters.append(letter)
     move_failing_letter(letter)
 
@@ -89,7 +96,6 @@ def move_failing_letter(letter):
                 falling_letters.remove(letter)
                 return
 
-
     # hit the wall
     for wall_cell in wall_cells:
         wall_cell_x, wall_cell_y, _, _ = canvas.coords(wall_cell)
@@ -107,7 +113,7 @@ def move_failing_letter(letter):
         falling_letters.remove(letter)
     else:
         canvas.move(letter, 0, 10)
-        canvas.after(200, move_failing_letter, letter)
+        canvas.after(500, move_failing_letter, letter)
 
 # User tank
 user_tank = canvas.create_rectangle(screen_width / 2, screen_height - 130, screen_width / 2 + 100, screen_height - 85, fill='brown')
@@ -251,15 +257,75 @@ def create_radars():
         initial_x1 += 480
 
 create_radars()
-print(radars)
 
-root.after(1000, move_tank)
+# Air defence
+def create_air_defense():
+    small_air_defence = canvas.create_rectangle(10, screen_height - 190, 30, screen_height - 210, fill='white', outline="blue")
+    big_air_defence = canvas.create_rectangle(screen_width - 20, screen_height - 190, screen_width - 40, screen_height - 210, fill='white', outline="red")
+
+    #setup rockets
+    global small_rocket
+    global big_rocket
+
+    small_rocket = canvas.create_rectangle(18, screen_height - 195, 22, screen_height - 205, fill='blue', outline="black")
+    big_rocket = canvas.create_rectangle(screen_width - 33, screen_height - 195, screen_width - 27, screen_height - 205, fill='red', outline="black")
+
+create_air_defense()
+
+def move_small_rocket(letter):
+    global small_rocket
+
+    letter_x, letter_y = canvas.coords(letter)
+    small_rocket_x, small_rocket_y, _, _ = canvas.coords(small_rocket)
+
+    if abs(small_rocket_x - letter_x) < 10 and abs(small_rocket_y - letter_y) < 10:
+        canvas.delete(small_rocket)
+        canvas.delete(letter)
+        falling_letters.remove(letter)
+        return
+
+    if small_rocket_y < 10:
+        canvas.delete(small_rocket)
+    else:
+        step_x = 10 if small_rocket_x < letter_x else -10  # Move 10 pixels towards the letter in X direction
+        step_y = -10 if small_rocket_y > letter_y else 10  # Move 10 pixels towards the letter in Y direction
+
+        canvas.move(small_rocket, step_x, step_y)
+        canvas.after(100, move_small_rocket, letter)
+def move_big_rocket(letter):
+    global big_rocket
+
+    letter_x, letter_y = canvas.coords(letter)
+    big_rocket_x, big_rocket_y, _, _ = canvas.coords(big_rocket)
+
+    if abs(big_rocket_x - letter_x) < 10 and abs(big_rocket_y - letter_y) < 10:
+        canvas.delete(big_rocket)
+        canvas.delete(letter)
+        falling_letters.remove(letter)
+        return
+
+    if big_rocket_y < 10:
+        canvas.delete(big_rocket)
+    else:
+        step_x = 10 if big_rocket_x < letter_x else -10  # Move 10 pixels towards the letter in X direction
+        step_y = -10 if big_rocket_y > letter_y else 10  # Move 10 pixels towards the letter in Y direction
+
+        canvas.move(big_rocket, step_x, step_y)
+        canvas.after(100, move_big_rocket, letter)
+
+def left_mouse_click(event, letter, is_uppercase):
+    if is_uppercase is not True:
+        move_small_rocket(letter)
+
+def right_mouse_click(event, letter, is_uppercase):
+    if is_uppercase:
+        move_big_rocket(letter)
 
 # Binders
+root.after(1000, move_tank)
 root.bind("<Left>", move_tank_left)
 root.bind("<Right>", move_tank_right)
 root.bind("<KeyPress-Up>", show_sight)
 root.bind("<KeyRelease-Up>", hide_sight)
 root.bind("<Key>", fire_letter)
-
 root.mainloop()
